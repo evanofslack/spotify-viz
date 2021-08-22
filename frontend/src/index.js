23f72a07344341ecb3c5ff95d5db9deae5bc0c87
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-08-22 13:22:32 -0400

    chakra provider moved to only the entry point

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import theme from "./themes/theme";
 
 ReactDOM.render(
-  <StrictMode>
-    <ColorModeScript />
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
+    <StrictMode>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript />
+            <App />
+        </ChakraProvider>
+    </StrictMode>,
+    document.getElementById("root")
 );
