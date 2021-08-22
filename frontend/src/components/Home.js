2b635e6bf79d76ea3511d1f4e95285870ce22a06
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-08-22 13:23:02 -0400

    refactored

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { ChakraProvider, Flex } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Flex, Box } from "@chakra-ui/react";
+import NavBar from "./NavBar";
 import HomeBlurb from "./HomeBlurb";
-import CurrentlyListening from "./CurrentlyListing";
-import theme from ".././themes/theme";
+import CurrentlyListening from "./CurrentlyListening";
+import LastListening from "./LastListening";
 
 function Home() {
     const [data, setData] = useState({
@@ -19,21 +19,27 @@ function Home() {
                     display_name: data.display_name,
                     current_song: data.current_song,
                     current_artist: data.current_artist,
+                    last_song: data.last_song,
+                    last_artist: data.last_artist,
+                    elapsed: data.elapsed,
                 });
             })
         );
     }, []);
 
-    const { display_name, current_song, current_artist } = data;
+    const { display_name, current_song, current_artist, last_song, last_artist, elapsed } = data;
 
     return (
-        <ChakraProvider theme={theme}>
-            <Flex align="center" justify="center" direction="column">
-                <ColorModeSwitcher justifySelf="flex-end" />
+        <Flex align="center" justify="center" direction="column">
+            <NavBar />
+            <Box>
                 <HomeBlurb name={display_name} />
                 {current_song && <CurrentlyListening song={current_song} artist={current_artist} />}
-            </Flex>
-        </ChakraProvider>
+                {!current_song && (
+                    <LastListening song={last_song} artist={last_artist} elapsed={elapsed} />
+                )}
+            </Box>
+        </Flex>
     );
 }
 
