4d5037055ec6f4d2b302800f182a111f28b02a97
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-08-23 22:24:52 -0400

    initialized sqlite db with sqlalchemy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,20 @@
 import uvicorn
 import tekore as tk
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
+from sqlalchemy.orm import Session
+from typing import List
+
 # from app.config import get_settings, Settings #___DOCKER___ "proxy": "http://host.docker.internal:8080",
 from config import get_settings, Settings
 from helpers.spotify import get_display_name, get_currently_playing, get_last_played
+
+from db import crud, models, schemas
+from db.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
@@ -23,6 +31,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 # file = './app/tekore.cfg' #___DOCKER___
@@ -44,10 +60,28 @@ def pong(settings: Settings = Depends(get_settings)):
     }
 
 
+@app.post("/users/")
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, spotify_id=user.spotify_id)
+    if db_user:
+        raise HTTPException(
+            status_code=400, detail="Spotify ID already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/")
+def read_users(db: Session = Depends(get_db)):
+    users = crud.get_all_users(db=db)
+    return users
+
+
 @app.get("/is_logged_in")
 def is_logged_in(request: Request):
     user = request.session.get('user', None)
     token = users.get(user, None)
+    '''
+    token = get_token_from_db(state: Str)
+    '''
 
     if user is None or token is None:
         request.session.pop('user', None)
@@ -102,6 +136,9 @@ def login(request: Request):
         tk.scope.user_read_playback_state + tk.scope.user_read_recently_played
     auth = tk.UserAuth(cred, scope)
     auths[auth.state] = auth
+    '''
+    update_user_db(auth.state, auth)
+    '''
     return {"url": auth.url}
 
 
