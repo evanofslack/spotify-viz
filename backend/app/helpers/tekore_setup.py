2dbaf4d8b56180fe9ddafc1a5775d5fca5250060
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-09-04 22:56:10 -0400

    moved tekore setup into new file

---
+++ backend/app/helpers/tekore_setup.py
@@ -0,0 +1,11 @@
+import tekore as tk
+
+file = 'tekore.cfg'  # file = './app/tekore.cfg'
+conf = tk.config_from_file(file)
+cred = tk.Credentials(*conf)
+spotify = tk.Spotify()
+
+scope = tk.scope.user_read_currently_playing + \
+    tk.scope.user_read_playback_state + tk.scope.user_read_recently_played
+
+auth = tk.UserAuth(cred, scope)
