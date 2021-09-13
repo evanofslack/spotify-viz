cebb03e1d92551e5977c171cb227d7607545b5b2
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-09-13 19:18:25 -0400

    don't drop tables

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -22,5 +22,5 @@ async def get_session() -> AsyncSession:
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.drop_all)
+        # await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
