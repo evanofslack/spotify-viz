2fdda8cfbf1d0f89d58889de62e38aaf8148e1ab
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-03 12:06:53 -0400

    main background grey changed

--- frontend/src/themes/theme.js
+++ frontend/src/themes/theme.js
@@ -11,7 +11,7 @@ const styles = {
     global: (props) => ({
         body: {
             color: mode("green.600", "#abb2bf")(props), //LIGHT, DARK
-            bg: mode("#f5f5f5", "#212022")(props), // atom_grey#282c34 # 25272B
+            bg: mode("#f5f5f5", "#3A3A3A")(props),
         },
     }),
 };
