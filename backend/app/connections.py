151f8599a66fbdc98dc5db4c5cbb9573a60e0cf7
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-01-25 20:30:10 -0500

    update redis class

--- backend/app/connections.py
+++ backend/app/connections.py
@@ -9,23 +9,29 @@ settings = get_settings()
 
 class RedisCache:
     def __init__(self):
-        self.redis_cache: Optional[aioredis.Redis] = None
+        self.redis: Optional[aioredis.Redis] = None
 
     async def init_cache(self):
-        self.redis_cache = await aioredis.from_url(settings.redis_url, encoding="utf-8")
+        self.redis = await aioredis.from_url(settings.redis_url, encoding="utf-8")
 
     async def keys(self, pattern):
-        return await self.redis_cache.keys(pattern)
+        return await self.redis.keys(pattern)
 
     async def set(self, key, value):
-        return await self.redis_cache.set(key, value)
+        return await self.redis.set(key, value)
 
     async def get(self, key):
-        return await self.redis_cache.get(key)
+        return await self.redis.get(key)
+
+    async def exists(self, key):
+        return await self.redis.exists(key)
 
     async def close(self):
-        self.redis_cache.close()
-        await self.redis_cache.wait_closed()
+        self.redis.close()
+        await self.redis.wait_closed()
+
+    async def hset(self, hash_name: str, key, value):
+        return await self.redis.hset(hash_name, key, value)
 
 
 redis_cache = RedisCache()
