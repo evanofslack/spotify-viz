d28b61e9585a961f6375c527e26458a5d5769128
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-01-27 00:57:03 -0500

    Fix useQuery fetch

--- frontend/src/components/home/PlaylistGallery.js
+++ frontend/src/components/home/PlaylistGallery.js
@@ -12,35 +12,36 @@ function PlaylistGallery() {
 
     if (error) return "An error has occurred: " + error.message;
 
-    return (
-        <Flex align="center" justify="center" direction="column">
-            <Box>
-                <Heading
-                    display="inline-block"
-                    align="left"
-                    mt="20"
-                    ml="2"
-                    color="#D2D2D2"
-                    fontWeight="300"
-                    fontSize="22px"
-                    borderBottom="1px solid #D2D2D2"
-                    paddingBottom="2px"
-                >
-                    Your mixes
-                </Heading>
+    if (data)
+        return (
+            <Flex align="center" justify="center" direction="column">
+                <Box>
+                    <Heading
+                        display="inline-block"
+                        align="left"
+                        mt="20"
+                        ml="2"
+                        color="#D2D2D2"
+                        fontWeight="300"
+                        fontSize="22px"
+                        borderBottom="1px solid #D2D2D2"
+                        paddingBottom="2px"
+                    >
+                        Your mixes
+                    </Heading>
 
-                {data.map((playlist, index) => {
-                    return (
-                        <PlaylistCard
-                            key={index}
-                            playlist_name={playlist.playlist_name}
-                            playlist_cover_image={playlist.playlist_cover_image}
-                        />
-                    );
-                })}
-            </Box>
-        </Flex>
-    );
+                    {data.map((playlist, index) => {
+                        return (
+                            <PlaylistCard
+                                key={index}
+                                playlist_name={playlist.playlist_name}
+                                playlist_cover_image={playlist.playlist_cover_image}
+                            />
+                        );
+                    })}
+                </Box>
+            </Flex>
+        );
 }
 
 export default PlaylistGallery;
